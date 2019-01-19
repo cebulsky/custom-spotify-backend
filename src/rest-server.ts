bff825cb14051c5a7d6562ef4c25d9a02c6bb1f1
Your Name <cebula.mat@gmail.com>
2019-01-19 16:13:46 +0100

    add bodyparser to middleware, install mongoose

--- src/rest-server.ts
+++ src/rest-server.ts
@@ -1,15 +1,14 @@
 import * as express from 'express'
 import { HomeController } from './controllers/home.controller';
 import { AlbumController } from './controllers/album.controller';
+import * as bodyParser from 'body-parser'
 
 export class RestServer {
-    private mountRoutes(): any {
-        this.app.use('/', HomeController)
-        this.app.use('/album', AlbumController)
-    }
+
     private app: express.Application = express();
 
     constructor() {
+        this.setupGlobalMiddleware();
         this.mountRoutes();
     }
 
@@ -17,5 +16,14 @@ export class RestServer {
         this.app.listen(port, function () {
             console.log(`REST server is working on localhost, port ${port}`)
         });
+    }
+
+    private setupGlobalMiddleware(): any {
+        this.app.use(bodyParser.urlencoded({ extended: true }))
+        this.app.use(bodyParser.json())
+    }
+    private mountRoutes(): any {
+        this.app.use('/', HomeController)
+        this.app.use('/album', AlbumController)
     }
 }
