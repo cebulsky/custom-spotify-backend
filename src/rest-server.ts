ab9fe5964d8b2394038cc011c3532d5c2729a83a
Your Name <cebula.mat@gmail.com>
2019-01-20 23:02:55 +0100

    add CORS

--- src/rest-server.ts
+++ src/rest-server.ts
@@ -3,6 +3,7 @@ import * as bodyParser from 'body-parser'
 import { ApiRouter } from './routing/api.router';
 import { HomeRouter } from './routing/home.router';
 import { connectToMongoDb } from './mongoDb';
+import * as cors from 'cors';
 
 export class RestServer {
 
@@ -25,6 +26,7 @@ export class RestServer {
     private setupGlobalMiddleware(): any {
         this.app.use(bodyParser.urlencoded({ extended: true }))
         this.app.use(bodyParser.json())
+        this.app.use(cors());
     }
     private mountRoutes(): any {
         this.app.use('/', HomeRouter)
