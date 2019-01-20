0d8155794b19aff08eb3716d78bfaa8a254941dc
Your Name <cebula.mat@gmail.com>
2019-01-20 19:30:04 +0100

    connection string change

--- src/mongoDb.ts
+++ src/mongoDb.ts
@@ -3,5 +3,5 @@ import mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
 export const connectToMongoDb = function connect() {
-    return mongoose.connect('mongodb://127.0.0.1:27017/mongodb');
+    return mongoose.connect('mongodb://127.0.0.1:27017');
 }
